e9e30aba7550a3d4106e907bda893a70b4adf783
Wafae <fouziwafae@gmail.com>
2017-10-18 14:49:59 +0200

    Charger Image

--- src/ImageViewer.java
+++ src/ImageViewer.java
@@ -1,22 +1,46 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.BoxLayout;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+import com.sun.javafx.tk.Toolkit;
+
+import javafx.scene.image.Image;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.MediaTracker;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
 
 public class ImageViewer extends JFrame /*implements ActionListener*/
 {
 	private DisplayedImage inputImage = new DisplayedImage(); 
 	private DisplayedImage ouputImage = new DisplayedImage();
 	private JButton buttonAction = new JButton("Action");
+	
+
 
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu fileMenu = new JMenu("File");
+	
+	private JMenuItem itemCharger = new JMenuItem("Charger"); //Ajouter "Charger" dans le menu
 
 	private JMenuItem itemClose = new JMenuItem("Close");
 
@@ -32,6 +56,9 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		JPanel action = new JPanel();
 		action.setLayout(new BoxLayout(action, BoxLayout.PAGE_AXIS));
 		action.add(buttonAction);
+		
+
+		
 		// Defines action associated to buttons
 		buttonAction.addActionListener(new ButtonListener());
 
@@ -51,14 +78,20 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 		itemClose.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
-			}        
+			} 
+			
+		
 		});
+		
+		itemCharger.addActionListener(new Chargement());
+		
 		this.fileMenu.add(itemClose);  
-
 		this.menuBar.add(fileMenu);
+		this.fileMenu.add(itemCharger);  
 		this.setJMenuBar(menuBar);
 
 		this.setVisible(true);
+		
 	}
 
 	/**
@@ -70,4 +103,36 @@ public class ImageViewer extends JFrame /*implements ActionListener*/
 			System.out.println("Action Performed");
 		}
 	}
-}
+	
+     class Chargement implements ActionListener{
+		public void actionPerformed(ActionEvent arg0)
+		{  				
+			JFileChooser ImageChooser = new JFileChooser();  //Selectionner
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("IMAGE" , "jpg" , "png" , "gif"); //Définir l'extention de l'image
+			ImageChooser.addChoosableFileFilter(filter); //Lien entre  le ImageChooser et le Filter pour definir l'extention que L'imageChooser doit avoir
+			int result = ImageChooser.showSaveDialog(null); //resultat de l'opération
+			
+			if (result == JFileChooser.APPROVE_OPTION) // Si on obtient le resultat souhaité
+			{
+				File SelectedImage = ImageChooser.getSelectedFile(); //Srocker l'image choisie dans un fichier
+				String Path = SelectedImage.getAbsolutePath(); // chemin de l'image selectionnée
+				ImageIcon MonImage = new ImageIcon(Path); // c est l image qui va etre utiliser dans l interface, on lui donne le chemin de l'image selectionnee
+			    java.awt.Image Img = MonImage.getImage();//Utiliser le format Image pour plusieurs options supp comme par exemple adapter la taille de l'image
+			    
+			    JLabel MyImag = new JLabel(new ImageIcon(Path));
+			   
+			
+			    add(MyImag, BorderLayout.WEST);
+			    pack();
+			    
+			    // Probleme de dimension et de position de l'image
+			    
+			}
+		}
+
+     }
+	}
+
+		
+	
+	
