0a79a762995357a859a61e89b26fb0cd76702c68
DepinfoCamille <camilletruong@orange.fr>
2017-10-19 14:55:39 +0200

    Création d'une classe Point

---
+++ src/Point.java
@@ -0,0 +1,14 @@
+
+public class Point {
+
+	public int red ; 
+	public int green ; 
+	public int blue ; 
+	
+	Point(int _red, int _green, int _blue){
+		
+		this.red = _red ; 
+		this.green = _green ; 
+		this.blue = _blue ;
+	}
+}
