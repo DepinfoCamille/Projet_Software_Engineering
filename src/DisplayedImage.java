eb77d6c7c8b031cdc41a87cf6380d18d974fb366
DepinfoCamille <camilletruong@orange.fr>
2017-10-19 14:57:00 +0200

    bêtise

--- src/DisplayedImage.java
+++ src/DisplayedImage.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import java.lang.Object ; 
  
 public class DisplayedImage extends JPanel {
 	
@@ -17,6 +18,10 @@ public class DisplayedImage extends JPanel {
         	}                
     }
     
+    public int getRGB(int x, int y) {
+    	return image.getRGB(x,y) ; 
+    }
+        
     public void paintComponent(Graphics g){
     		//g.drawImage(image, 0, 0, this); // draw as much as possible
     		g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this); // draw full image
