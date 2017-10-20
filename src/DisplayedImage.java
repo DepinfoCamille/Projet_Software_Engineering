83bded02b3d447b17968697c66394e77e73472bb
Wafae <fouziwafae@gmail.com>
2017-10-20 22:34:51 +0200

    Charger

--- src/DisplayedImage.java
+++ src/DisplayedImage.java
@@ -1,13 +1,27 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+import javafx.scene.image.Image;
  
 public class DisplayedImage extends JPanel {
 	
-    private BufferedImage image;
+   
+	private BufferedImage image;
+    
     
     public DisplayedImage() {
     		try {
@@ -20,5 +34,17 @@ public class DisplayedImage extends JPanel {
     public void paintComponent(Graphics g){
     		//g.drawImage(image, 0, 0, this); // draw as much as possible
     		g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this); // draw full image
+    	    
     }                   
-}
+    
+   public void setImage(BufferedImage res)
+   {
+	   image = res;
+    }
+   
+   public BufferedImage getImage() {
+   	return image;
+   }
+	  
+   }
+   
