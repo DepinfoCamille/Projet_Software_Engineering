2a3264ceba2e319993abfa5908bdcdbc6061421b
DepinfoCamille <camilletruong@orange.fr>
2017-10-24 23:54:41 +0200

    Fonctions getParent et removePoint fonctionnelles !

--- src/ArbreBinaire.java
+++ src/ArbreBinaire.java
@@ -29,37 +29,25 @@ public class ArbreBinaire {
 		this.dim = 1 ;
 	}
 	
-	boolean estNul(){
-		return (this==null || this.racine==null); 
-	}
-	
-	public String toString() {
+/*	public String toString() {
 		
-		if (this.racine ==null) {
-			return "" ; 
-		} 
-		
-		else{
-			if((this.filsDroit.racine==null) & (this.filsGauche.racine==null)) {
-				return this.racine.point.toString() ; 
+		if((this.filsGauche.estNul()) & (this.filsDroit.estNul())) { // initialisation : on est au niveau d'une feuille
+			return this.etiquette.point.toString() ;
+		}
+			
+		else {
+			if(this.filsGauche.estNul()) {
+				return this.etiquette.point.toString()+" ( null , " + this.filsDroit.toString() + " ) " ; 
 			}
-			
-			else {
-			
-				if(this/*.racine*/.filsGauche.racine==null) {
-					return this.racine.point.toString()+" ( null , " + this.filsDroit.toString() + " ) " ;
-				}
-			
-				if(this/*.racine*/.filsDroit.racine==null) {
-					return this.racine.point.toString()+" ( " + this.filsGauche.toString()+ " ,  null ) " ;
-				}
 		
-				else {
-					return this.racine.point.toString()+" ( " +this.filsGauche.toString()+ " , " + this.filsDroit.toString() + " ) " ; 
-				}
+			if(this.filsDroit.estNul()) {
+				return this.etiquette.point.toString()+" ( " + this.filsGauche.toString()+ " , null ) " ; 
+			}
+			else {
+				return this.etiquette.point.toString()+" ( " +this.filsGauche.toString()+ " , " + this.filsDroit.toString() + " ) " ; 
 			}
 		}
-	} 
+	} */
 		
 	boolean estaGauche(Point point, int indice) { // cette fonction permet de déterminer si le point en entrée est  dans le fils gauche ou droit de l'arbre initial (this). A chaque noeud est associé ou bien l'hyperplan (x0...,xdim-1), ou bien l'hyperplan (x1...xdim)
 				
@@ -74,23 +62,11 @@ public class ArbreBinaire {
 		
 	}
 			
-	void addPointAux(Point point, int i) { //ajoute un point à l'arbre, cette fonction auxiliaire permet de gardr l'indice i correspondant à l'hyperplan du père en mémoire
+	void addPointAux(Point point, int i) { //ajoute un point à l'arbre	
 		
-		if (this.racine==null) { // l'emplacement est libre, on peut donc créer une feuille noeud
-			System.out.println("Création de feuille") ; 
-			
+		if (this.racine==null) { // l'arbre est vide
+			this.racine = new Noeud(point) ; 
 			this.dim = point.dim ; 
-
-	
-			this.racine = new Noeud(point) ; // initialisation du nouveau noeud et de ses fils
-			System.out.println(this.racine.point) ; 
-
-			System.out.println(point) ; 
-			this.filsGauche = new ArbreBinaire() ; 
-			this.filsGauche.racine = null ; 
-			this.filsDroit = new ArbreBinaire() ; 
-			this.filsDroit.racine = null ; 
-
 			
 			if(i==this.dim){
 				this.racine.indice = 1 ;
@@ -102,50 +78,16 @@ public class ArbreBinaire {
 		
 		else {
 			if(this.estaGauche(point, this.racine.indice)) {
-				System.out.println("on est à gauche") ; 
-
 				this.filsGauche.addPointAux(point, i+1) ; 
 			}
 			else {
-				System.out.println("on est à droite") ; 
-
 				this.filsDroit.addPointAux(point, i+1) ; 
 			}			
 		}
 	}
 	
-	void addPoint(Point point){ // c'est la véritable fonction addPoint, qui est la fonction addPointAux initialisée à 1 (i.e la racine de l'arbre a un hyperplan d'indice 1)
+	void addPoint(Point point){
 		addPointAux(point, 1) ; 
 	}
 		
-	Noeud getParentAux(Noeud noeud, Noeud pere) { // fonction auxiliaire qui regarde si le noeud correspondant est bien égal au père en entrée
-		
-		if(this.racine ==pere){
-			return this.racine ; 
-		}
-		
-		else {
-			if(estaGauche(noeud.point, this.racine.indice)){ 
-				return getParentAux(noeud,this.filsGauche.racine);  
-			}
-			else { 
-				return getParentAux(noeud,this.filsDroit.racine);  
-			}
-		}		
-	}
-	
-	Noeud getParent(Noeud noeud) { // véritable getParent
-		if(estaGauche(noeud.point, this.racine.indice)){ 
-			return getParentAux(noeud,this.filsGauche.racine);  
-		}
-		else { 
-			return getParentAux(noeud,this.filsDroit.racine);  
-		}
-	}
-	
-	void removePoint(Point point) {
-		
-		
-		
-	}
 }
