2c5f40043af22c60690d8c97b7a9e753d2a33f18
yosra <dirgham.yosra@gmail.com>
2017-10-26 22:50:47 +0200

    ajout de la fonction getcoord()+modification de la fonction distance+j'ai mis Tostring en commentaire pcq on a deja la fct getcoord qui fait la mm chose

--- src/Point.java
+++ src/Point.java
@@ -1,55 +1,76 @@
+
+import java.lang.Math;
+
 public class Point {
 	
-	protected int coordonnees[], dim ; 
+	
+	protected int coordonnees[];
+	int dim ; 
+	
+	
 	
 	Point(int coord[]) {
 		this.coordonnees = coord ; 
 		this.dim = this.coordonnees.length; 
 	}
 	
+	
+	public int[] getCoord() {	
+		int x,i;
+		x=this.dim;
+		int T[]= new int[x];
+		for(i=0;i<dim;i++) {
+		T[i]=this.coordonnees[i];
+	}
+		return T;
+	}
+	
+	
 	public int getCoord(int x) {
 		return this.coordonnees[x];
 
-	}
-		
-	public float distance(Point p) {
-		
-		float dist = 0;
-		
-		for (int i = 0 ; i < this.dim ; i++) {
-			
-			dist = dist + (this.coordonnees[i] - p.coordonnees[i])*(this.coordonnees[i] - p.coordonnees[i]);
-			
-		}
-		
-		return dist;
-	}
+	}  
 	
-	float distanceAxe(Point point, int axe) { // renvoie la distance entre deux points selon l'axe en entrée
+	
+	
+	int distanceAxe(Point point, int axe) {
 		return point.getCoord(axe)-this.getCoord(axe) ; 
 	}
 	
-	public String toString() { // affiche un point 
-		String chaine = "["+this.coordonnees[0] ; 
-		for (int i =1 ; i<(this.coordonnees).length ; i++) {
-			chaine += ","+Integer.toString(this.coordonnees[i]) ;
-		}
-		chaine+= "]" ;
-		
-		return chaine ;
+	
+	public int distance(Point p) {
+		int dist = 0,distance;
+		int x=this.dim;
+		for (int i = 0 ; i < x; i++) {
+			dist = dist + (this.coordonnees[i] - p.coordonnees[i]) ^ 2;
+			}
+		distance=(int) Math.sqrt(dist);
+		return distance;
 	}
 
+	
+
+
 	public boolean egalite(Point p) {
-		
 		for(int i = 0 ; i < coordonnees.length ; i++) {
-			
 			if (this.coordonnees[i] != p.coordonnees[i]) {
 				return false;
 			}
-			
 		}
-		
 		return true;
 	}
 }
 
+
+
+
+
+/*public String toString() { // affiche un point 
+	String chaine = "["+this.coordonnees[0] ; 
+	for (int i =1 ; i<(this.coordonnees).length ; i++) {
+		chaine += ","+Integer.toString(this.coordonnees[i]) ;
+	}
+	chaine+= "]" ;
+	
+	return chaine ;
+}*/
