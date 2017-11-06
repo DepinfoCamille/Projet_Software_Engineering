f9b6620e12205ab5923779db64058b86960a8c10
yosra <dirgham.yosra@gmail.com>
2017-11-06 21:44:08 +0100

    histogram...done :)

--- src/Histogramme.java
+++ src/Histogramme.java
@@ -1,17 +1,12 @@
-import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.Paint;
-import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.AbstractAction;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -25,11 +20,13 @@ import org.jfree.data.statistics.HistogramDataset;
 
 public class Histogramme {
 	 private static final int BINS = 256;
-	    private BufferedImage image = getImage();
-	    private HistogramDataset dataset;
+	    private static BufferedImage image = getImage();
+	    private static HistogramDataset dataset;
 	    private XYBarRenderer renderer;
 
-	    private BufferedImage getImage() {
+/************************************Selecting the buffered Image************************************************************************/	    
+	  
+	    private static BufferedImage getImage() {
 	        	try {
 	    			image = ImageIO.read(new File("img.png"));
 	        	} catch (IOException e) {
@@ -37,32 +34,42 @@ public class Histogramme {
 	        	}
 				return image;  
 	    }
+	    
+	    
+/**********************************Extracting the values of each color on the "img.png" image*******************************************/
 
-	    private ChartPanel createChartPanel() {
-	        // dataset
+	    static HistogramDataset ExtractColorValues(HistogramDataset dataset) {  
+     
+	    Raster raster = image.getRaster();
+     final int w = image.getWidth();
+     final int h = image.getHeight();
+     double[] r = new double[w * h];
+     r = raster.getSamples(0, 0, w, h, 0, r);
+     dataset.addSeries("Red", r, BINS);
+     r = raster.getSamples(0, 0, w, h, 1, r);
+     dataset.addSeries("Green", r, BINS);
+     r = raster.getSamples(0, 0, w, h, 2, r);
+     dataset.addSeries("Blue", r, BINS);
+     return dataset;
+     
+	   }
+	    
+/*************************************Creating a charPanel *****************************************************************************/	    
+	    
+	   private ChartPanel createChartPanel() {
 	        dataset = new HistogramDataset();
-	        Raster raster = image.getRaster();
-	        final int w = image.getWidth();
-	        final int h = image.getHeight();
-	        double[] r = new double[w * h];
-	        r = raster.getSamples(0, 0, w, h, 0, r);
-	        dataset.addSeries("Red", r, BINS);
-	        r = raster.getSamples(0, 0, w, h, 1, r);
-	        dataset.addSeries("Green", r, BINS);
-	        r = raster.getSamples(0, 0, w, h, 2, r);
-	        dataset.addSeries("Blue", r, BINS);
+	        ExtractColorValues(dataset);
+	        
 	        // chart
-	        JFreeChart chart = ChartFactory.createHistogram("Histogram", "Value",
-	            "Count", dataset, PlotOrientation.VERTICAL, true, true, false);
+	        
+	        JFreeChart chart = ChartFactory.createHistogram("Histogram", "Value","Count", dataset, PlotOrientation.VERTICAL, true, true, false);
 	        XYPlot plot = (XYPlot) chart.getPlot();
 	        renderer = (XYBarRenderer) plot.getRenderer();
 	        renderer.setBarPainter(new StandardXYBarPainter());
+
 	        // translucent red, green & blue
-	        Paint[] paintArray = {
-	            new Color(0x80ff0000, true),
-	            new Color(0x8000ff00, true),
-	            new Color(0x800000ff, true)
-	        };
+	        
+	       Paint[] paintArray = {new Color(0x80ff0000, true),new Color(0x8000ff00, true),new Color(0x800000ff, true)};
 	        plot.setDrawingSupplier(new DefaultDrawingSupplier(
 	            paintArray,
 	            DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
@@ -74,37 +81,13 @@ public class Histogramme {
 	        panel.setMouseWheelEnabled(true);
 	        return panel;
 	    }
+	    
 
-	    private JPanel createControlPanel() {
-	        JPanel panel = new JPanel();
-	        panel.add(new JCheckBox(new VisibleAction(0)));
-	        panel.add(new JCheckBox(new VisibleAction(1)));
-	        panel.add(new JCheckBox(new VisibleAction(2)));
-	        return panel;
-	    }
-
-	    private class VisibleAction extends AbstractAction {
-
-	        private final int i;
-
-	        public VisibleAction(int i) {
-	            this.i = i;
-	            this.putValue(NAME, (String) dataset.getSeriesKey(i));
-	            this.putValue(SELECTED_KEY, true);
-	            renderer.setSeriesVisible(i, true);
-	        }
-
-	        @Override
-	        public void actionPerformed(ActionEvent e) {
-	            renderer.setSeriesVisible(i, !renderer.getSeriesVisible(i));
-	        }
-	    }
-
+/**************************************exhibiting the histogram******************************************************************************/
+	    
 	    void display() {
 	        JFrame f = new JFrame("Histogram");
-	        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        f.add(createChartPanel());
-	        f.add(createControlPanel(), BorderLayout.SOUTH);
 	        f.pack();
 	        f.setLocationRelativeTo(null);
 	        f.setVisible(true);
